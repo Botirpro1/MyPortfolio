6cb24e7f8db27b639b6d153eaf941da9f4a777b6
Botirpro1 <125514647+Botirpro1@users.noreply.github.com>
2025-09-12 01:04:57 +0500

    fix: ensure src/shared/styles/fonts.css exists and import path correct

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/app/App.jsx";
-import "@/shared/assets/style/main.css";
+import "@/shared/assets/styles/main.css";
 import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./app/providers/theme";
 
